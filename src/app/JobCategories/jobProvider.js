56afd71dbb15fda8af00f74f05d34d1a5ad2b4ca
민욱 <alsdnrdl001@gmail.com>
2022-07-19 02:20:24 +0900

    API 6번 ERROR해결과 Response에서 skills 제외

--- src/app/JobCategories/jobProvider.js
+++ src/app/JobCategories/jobProvider.js
@@ -12,19 +12,13 @@ exports.getJobCategories = async function (jobGroupId) {
         const connection = await pool.getConnection(async (conn) => conn);
         
         const jobCategoryRows=await jobDao.getJobCategories(connection,jobGroupId);
-        console.log(jobCategoryRows[0]);
-        if(jobGroupId==1){
-            skills=await skillDao.getSkillsAll(connection);    //TODO skill Domain으로 옮겨야함.
-        }else{
-            skills=null;
-        }
+    
         connection.release();
 
-        const ResultRows={};
-        ResultRows.jobCategories=jobCategoryRows[0];
-        ResultRows.skills=skills[0];
+        
+       
 
-        return ResultRows;
+        return jobCategoryRows[0];
     }catch(err){
         logger.error(`App - Get Job Categories Service error\n: ${err.message}`);
         return errResponse(baseResponse.DB_ERROR);
