e8f7f0d60321fbf8c46e6066e1352bc157dd8ac2
민욱 <alsdnrdl001@gmail.com>
2022-07-22 02:35:15 +0900

    인사이트 태그 전체 조회 API 구축

--- src/app/PostTag/postTagDao.js
+++ src/app/PostTag/postTagDao.js
@@ -1,7 +1,8 @@
 // 모든 유저 조회
-async function getPostTags(connection,jobGroupId) {
+async function getPostTags(connection) {
     const getPostTagsQuery = `
-            select tagId,name from postTags;
+        select tagId,name from postTags
+        where postTags.region%2=0;
                   `;
     const PostTagsRow = await connection.query(getPostTagsQuery);
     return PostTagsRow;
